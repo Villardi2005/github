8dd587543833aceee39b1ede25529dd44bd6c782
rafandrade1000 <rafabumandrade@gmail.com>
2025-06-13 12:17:00 -0300

    Update admin.py

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -5,3 +5,10 @@ from .models import Professor
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ['nome', 'email']
     search_fields = ['nome', 'email']
+
+from .models import Usuario
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'nome_completo', 'telefone']
+
