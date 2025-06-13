7c3e37eabf88d8e8e326efe750baf6ae6ae79629
Villardi2005 <gabrielvillardi@gmail.com>
2025-06-13 16:52:46 +0000

    admin e models

--- evasao/admin.py
+++ evasao/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Usuario
+from django.contrib import admin
+from .models import Professor
+from .models import Responsavel
 
-# Register your models here.
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email']
+    search_fields = ['nome', 'email']
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'nome_completo', 'telefone']
+
+admin.site.register(Responsavel)
--- evasao/models.py
+++ evasao/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.nome
+
+class Usuario(AbstractUser):
+    nome_completo = models.CharField(max_length=150, blank=True)
+    telefone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
+        
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    idade = models.IntegerField()
+    matricula = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Responsavel(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.nome
