fb8a7e9f7a3a662cecb141395357a02b926fc982
rafandrade1000 <rafabumandrade@gmail.com>
2025-06-13 12:14:45 -0300

    Update urls.py

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -4,3 +4,12 @@ from . import views
 urlpatterns = [
     path('cadastroprofessor/', views.cadastro_professor, name='cadastro_professor'),
 ]
+from django.contrib import admin
+from django.urls import path
+from hello_world.core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),  
+]
+
